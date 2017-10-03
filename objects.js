e5965cb74738af7e0b6ec91229500186543555fd
savita madray <savitamadray01@gmail.com>
2017-10-03 22:53:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi"){
-  playlist[playlist]: object
+  playlist[Phil Ochs]: "Here's to the State of Mississippi"
 
   return playlist
 };
